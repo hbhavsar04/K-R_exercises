17b3bc9babb20806053ad43a44c22331951ed7c8
Hayden Bhavsar <hbhavsar@uw.edu>
2026-02-21 01:12:27 -0800

    started exercise 2-9, logic is there I think, but I made some mistake. Will come back to it later.

---
+++ 2_9_bitwise_operators/invert.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	unsigned invert(unsigned x, int p, int n);
+	unsigned num = 170;
+	printf("invert results %u\n expected result: 154", invert(num, 4, 2));
+}
+
+// returns x with the n bits that begin at position p inverted
+unsigned invert (unsigned x, int p, int n)
+{
+	return ((((~x) >> (p+1-n) & ~(~0 << n)) << p) | (x & ~((~(~0 << n)) << p)));
+}
