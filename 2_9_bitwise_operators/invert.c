69e9f478816ff97e620e2865ee82b03f98c53ef6
Hayden Bhavsar <hbhavsar@uw.edu>
2026-02-21 17:08:00 -0800

    Figured out invert. Turns out it was an order of operations problem.

--- 2_9_bitwise_operators/invert.c
+++ 2_9_bitwise_operators/invert.c
@@ -10,5 +10,5 @@ int main()
 // returns x with the n bits that begin at position p inverted
 unsigned invert (unsigned x, int p, int n)
 {
-	return ((((~x) >> (p+1-n) & ~(~0 << n)) << p) | (x & ~((~(~0 << n)) << p)));
+	return (((((~x) >> (p+1-n)) & ~(~0 << n)) << p) | (x & ~((~(~0 << n)) << p)));
 }
