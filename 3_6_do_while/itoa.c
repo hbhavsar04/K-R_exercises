12d9aea2a53e52648a00be006869078000d2c94f
Hayden Bhavsar <hbhavsar@uw.edu>
2026-02-24 23:02:12 -0800

    worked on 3_6

---
+++ 3_6_do_while/itoa.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#define MAXLEN 1000
+int main()
+{
+	char string[MAXLEN];
+	void iota(int n, char s[]);
+	int i;
+	
+	for (i = 0; i < MAXLEN; ++i){
+		string[i] = '\0';
+	}
+	iota(12, string);
+	for (i = 0; string[i] != '\0'; ++i)
+		putchar(string[i]);
+}
+// iota: convert n to characters in s
+void iota (int n, char s[])
+{
+	int i, sign;
+	void reverse(char s[]);
+	if ((sign = n) < 0) // record the sign
+		n = -n; // make it positive
+	i = 0;
+	do { // generate digits in reverse order
+		s[i++] = n % 10 + '0'; // get the next digit
+	} while ((n /= 10) > 0); // delete it
+	if (sign < 0)
+		s[i++] = '-';
+	s[i] = '\0';
+	reverse(s);
+}
+void reverse(char s[]) 
+{
+	int i, j;
+	char temp[MAXLEN];
+	for (i = 0; s[i] != '\0'; ++i)
+		temp[i] = s[i];
+	--i;
+	for (j = 0; i >= 0; ++j){
+		s[j] = temp[i];
+		--i;
+	}
+	s[j] = '\0';
+}
---
+++ 3_6_do_while/notes.md
@@ -0,0 +1,9 @@
+# Do-while notes
+In contrast to the other loops so far, do while tasts at the bottom after making each pass through the loop body
+This means the body is always executed at least once
+## syntax
+do
+	statement
+while (expression);
+
+do while tends to be less used than while and for
