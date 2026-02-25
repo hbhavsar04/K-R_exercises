4fdc8443c22e68c26a1ef0ed24b8463a6e110825
Hayden Bhavsar <hbhavsar@uw.edu>
2026-02-24 20:12:31 -0800

    finished exercise 3_4

--- 3_4_switch/escape.c
+++ 3_4_switch/escape.c
@@ -1,4 +1,4 @@
-// write a function escape(s,t) that converts characters like newline and tab into visible
+// write a function escape(s,t) that converts characters like newline and tabs into visible
 // escape sequences like \n and \t as it copies the string t to s. Use a switch.
 // write a function for ther other direction as well, converting escape seqences into the real characters
 
@@ -8,17 +8,58 @@
 int main()
 {
 	void escape(char s[], char t[]);
+	void reverse_escape(char s[], char t[]);
 	char string[MAXLEN];
 	char tabbed_string[MAXLEN];
+	char added_string[MAXLEN];
 	int i, c, j;
+	
 	for (i = 0; i < MAXLEN - 1 && (c = getchar()) != EOF; ++i)
 		string[i] = c;
 	string[i] = '\0';
+	
 	escape(string, tabbed_string);
 	for (j = 0; j < MAXLEN - 1 && (tabbed_string[j] != '\0'); ++j)
 		putchar(tabbed_string[j]);
-}
+	putchar('\n');
 
+	for (i = 0; i < MAXLEN-1; ++i)
+		added_string[i] = 0;
+
+	reverse_escape(tabbed_string, added_string);
+	for (j = 0; j < MAXLEN - 1 && (added_string[j] != '\0'); ++j)
+		putchar(added_string[j]);
+
+}
+void reverse_escape (char s[], char t[])
+{
+	int i, j;
+	j = i = 0;
+	while (s[i] != '\0'){
+		switch (s[i]) {
+			case '\\':
+				switch (s[i+1]){
+					case 't':
+						t[j] = '\t';
+						j++;
+						i += 2;
+						break;
+					case 'n':
+						t[j] = '\n';
+						j++;
+						i += 2;
+						break;
+				}
+				break;
+			default:
+				t[j] = s[i];
+				j++;
+				i++;
+				break;
+		}
+	}
+	t[i] = '\0';
+}
 void escape(char s[], char t[])
 {
 	int i = 0;
