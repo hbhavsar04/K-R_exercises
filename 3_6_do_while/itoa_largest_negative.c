a7758af14687abbec9c3d54b284b58a5f5ff45b9
Hayden Bhavsar <hbhavsar@uw.edu>
2026-02-25 23:28:04 -0800

    added exercise 3_4

--- 3_6_do_while/itoa_largest_negative.c
+++ 3_6_do_while/itoa_largest_negative.c
@@ -2,6 +2,21 @@
 #define MAXLEN 1000
 
 // itoa does not hadnle the largest negative number (in a twos complement system)
+// that would be something like 
+// 10000000
+// in an 8 bit number
+// explain why not and modify it to print that value correctly, regardless of the machine on which it runs
+//
+// First let's see what happens when I try the largest negative number for an int (-2147483648)
+//
+// for -2147483647
+// it printed -2147483647
+//
+// for -2147483648
+// it printed -(
+//
+//hmm I think it has to do with the modulo operator?
+// no actually I think it's the check where we make it positive since 2147483648 would overflow
 
 int main()
 {
@@ -12,21 +27,25 @@ int main()
 	for (i = 0; i < MAXLEN; ++i){
 		string[i] = '\0';
 	}
-	iota(12, string);
+	iota(-2147483648, string);
 	for (i = 0; string[i] != '\0'; ++i)
 		putchar(string[i]);
 }
 // iota: convert n to characters in s
 void iota (int n, char s[])
 {
-	int i, sign;
+	int i, sign, m;
 	void reverse(char s[]);
-	if ((sign = n) < 0) // record the sign
-		n = -n; // make it positive
+	
+	sign = n; // record the sign
+	m = 0;
 	i = 0;
 	do { // generate digits in reverse order
-		s[i++] = n % 10 + '0'; // get the next digit
-	} while ((n /= 10) > 0); // delete it
+	     	m = n % 10;
+		if (m < 0)
+			m = -m;
+		s[i++] = m + '0'; // get the next digit
+	} while ((n /= 10) != 0); // delete it
 	if (sign < 0)
 		s[i++] = '-';
 	s[i] = '\0';
